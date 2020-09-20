03b2ba36ce7e6297c687f8ba5b34e4454ad2d0f6
Gunbileg <gunbileg92@gmail.com>
2020-09-20 23:50:33 +0800

    Updates

---
+++ precache-manifest.f63f9a1fdaf4b09c08daabf7cb5e7b87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5954e17b3404d1b2916f9ca177a003b7",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "6c6a4d201846ac28a9e6",
+    "url": "/burger/static/css/main.4fdd7a8a.chunk.css"
+  },
+  {
+    "revision": "ccee51e6e75ed84eb809",
+    "url": "/burger/static/js/2.886a8dda.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/burger/static/js/2.886a8dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6a4d201846ac28a9e6",
+    "url": "/burger/static/js/main.f39ee8eb.chunk.js"
+  },
+  {
+    "revision": "1230e3bd6a4b2a4ff75f",
+    "url": "/burger/static/js/runtime-main.68853aa2.js"
+  },
+  {
+    "revision": "fae5429cd86ba24bd19cfe6217cd4562",
+    "url": "/burger/static/media/logo.fae5429c.png"
+  }
+]);
